ed2e8f68401bac47a40e7d3014e0702ec894ea2f
CuongND <cuongnd@archergame.mobi>
2025-11-04 09:50:16 +0700

    Implement object_pool core and config file

---
+++ assets/configs/constant.ts
@@ -0,0 +1,22 @@
+﻿export class constant {
+    static STORE_LINK = {
+        ANDROID_LINK: 'https://play.google.com/',
+        IOS_LINK: 'https://apps.apple.com/',
+    }
+
+    static AUDIO_NAME = {
+        BGM: 'BGM',              
+        WIN: 'Win',              
+        LOSE: 'Lose',            
+        INPUT: 'Input',          
+        SHOOT: 'shoot',          
+        DEFAULT: ''
+    }
+
+    static EFFECT_NAME = {
+        EFFECT_TEST: 'effect_ex',
+        EFFECT_EX: 'effect_ex',
+        DEFAULT: ''
+    }
+
+}
